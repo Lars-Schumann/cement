20cd51d1ed0a372806fbb8384dace5972aaeedfc
Lars Schumann <LarsSchumann@mail.de>
2026-04-06 23:25:35 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "rust dev shell for vsnymit";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+
+          clippy
+          bacon
+
+          nixfmt
+          nixd
+
+        ];
+        env = {
+          RUST_BACKTRACE = "1";
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+
+        shellHook = ''
+          export PS1="\[\e[1;33m\][vsnymit]]\[\e[0m\] $PS1"
+        '';
+      };
+    };
+}
