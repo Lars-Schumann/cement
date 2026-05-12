34f3189512a034be7baff8b9c82798e034899204
Lars Schumann <LarsSchumann@mail.de>
2026-05-12 23:06:44 +0200

    fix: left over manual type assert

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,6 @@ mod tests {
         auto_array!(
             pub const ARRAY_1: [i32; _] = [1, 2, 3];
         );
-        const _: [i32; 3] = ARRAY_1;
         assert_type!(ARRAY_1, [i32; 3]);
         assert_eq!(ARRAY_1, [1, 2, 3]);
 
@@ -30,7 +29,7 @@ mod tests {
             pub(super) static ARRAY_2: [i32; _] = [4, 4, 4, 4];
         );
         assert_type!(ARRAY_2, [i32; 4]);
-        assert_eq!(ARRAY_2, [4, 4, 4, 4])
+        assert_eq!(ARRAY_2, [4, 4, 4, 4]);
     }
 
     #[test]
