83959603d4dfa93d50f51489b5e15e77514670fc
Lars Schumann <LarsSchumann@mail.de>
2026-05-12 19:30:26 +0200

    fix example

--- examples/example.rs
+++ examples/example.rs
@@ -1,26 +1,13 @@
-#![feature(const_heap)]
-#![feature(const_trait_impl)]
-#![feature(maybe_uninit_array_assume_init)]
-
 use auto_array::auto_array;
 
 fn main() {
-    #[expect(clippy::vec_init_then_push)]
-    const fn one_two_three() -> Vec<i32> {
-        let mut vec: Vec<i32> = Vec::new();
-        vec.push(1);
-        vec.push(2);
-        vec.push(3);
-        vec
-    }
+    auto_array!(
+        pub const ARRAY1: [i32; _] = [1, 2, 3];
+    );
+    auto_array!(
+        static ARRAY2: [i32; _] = [4, 4, 4, 4];
+    );
 
-    auto_array!(pub const ARRAY1: [i32; ?] = one_two_three());
-    auto_array!(static ARRAY2: [i32; ?] = one_two_three());
-
-    auto_array!(pub(in self) const ARRAY3: [i32; 3] = one_two_three());
-    auto_array!(pub static ARRAY4: [i32; 3] = one_two_three());
-
-    auto_array!(static ARRAY5: &[i32] = one_two_three());
-
-    dbg!(ARRAY1, ARRAY2, ARRAY3, ARRAY4, ARRAY5);
+    assert_eq!(ARRAY1, [1, 2, 3]);
+    assert_eq!(ARRAY2, [4, 4, 4, 4]);
 }
