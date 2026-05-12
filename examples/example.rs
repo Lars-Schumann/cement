0bd82a62db5b47c6f3438890d6aa8fea425c3ad4
Lars Schumann <LarsSchumann@mail.de>
2026-05-13 00:36:06 +0200

    chore: sync examples

--- examples/example.rs
+++ examples/example.rs
@@ -2,10 +2,9 @@ use auto_array::auto_array;
 
 fn main() {
     auto_array!(
-        pub const ARRAY1: [i32; _] = [1, 2, 3];
-        static ARRAY2: [u8; _] = [4, 4, 4, 4];
+        pub const A: [i32; _] = [1, 2, 3];
+        static B: [u8; _] = [4, 4, 4, 4];
     );
-
-    assert_eq!(ARRAY1, [1, 2, 3]);
-    assert_eq!(ARRAY2, [4, 4, 4, 4]);
+    assert_eq!(A, [1, 2, 3]);
+    assert_eq!(B, [4, 4, 4, 4]);
 }
