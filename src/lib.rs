3fe4206d44ec82183f54e8347b999964e791c2bd
Lars Schumann <LarsSchumann@mail.de>
2026-05-12 22:30:37 +0200

    feat: make multiple items in the macro possible

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@
 
 #[macro_export]
 macro_rules! auto_array {
-    ($vis:vis const $name:ident: [$ty:ty; _] = $array:expr;) => {
-        $vis const $name: [$ty;<[$ty]>::len(&($array))] = $array;
+    ($($vis:vis const $name:ident: [$ty:ty; _] = $array:expr;)+) => {
+        $($vis const $name: [$ty;<[$ty]>::len(&($array))] = $array;)+
     };
-    ($vis:vis static $name:ident: [$ty:ty; _] = $array:expr;) => {
-        $vis static $name: [$ty; <[$ty]>::len(&($array))] = $array;
+    ($($vis:vis static $name:ident: [$ty:ty; _] = $array:expr;)+) => {
+        $($vis static $name: [$ty; <[$ty]>::len(&($array))] = $array;)+
     };
 }
 
@@ -33,5 +33,32 @@ mod tests {
         );
         assert_type!(ARRAY_2, [i32; 4]);
         assert_eq!(ARRAY_2, [4, 4, 4, 4])
+    }
+
+    #[test]
+    fn multiple_arrays() {
+        auto_array!(
+            pub const ARRAY_1: [i32; _] = [1, 2, 3];
+            pub const ARRAY_2: [i32; _] = [];
+            pub const ARRAY_3: [i32; _] = [4, 4, 4, 4];
+            pub const ARRAY_4: [i32; _] = [6, 6, 6, 6, 6, 6];
+        );
+        assert_type!(ARRAY_1, [i32; 3]);
+        assert_type!(ARRAY_2, [i32; 0]);
+        assert_type!(ARRAY_3, [i32; 4]);
+        assert_type!(ARRAY_4, [i32; 6]);
+        assert_eq!(ARRAY_1, [1, 2, 3]);
+        assert_eq!(ARRAY_2, []);
+        assert_eq!(ARRAY_3, [4, 4, 4, 4]);
+        assert_eq!(ARRAY_4, [6, 6, 6, 6, 6, 6]);
+
+        auto_array!(
+            pub static ARRAY_5: [i32; _] = [1];
+            pub static ARRAY_6: [i32; _] = [2, 2];
+        );
+        assert_type!(ARRAY_5, [i32; 1]);
+        assert_type!(ARRAY_6, [i32; 2]);
+        assert_eq!(ARRAY_5, [1]);
+        assert_eq!(ARRAY_6, [2, 2]);
     }
 }
