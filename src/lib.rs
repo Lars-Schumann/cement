f7ec45a990e2dbaad54b56cb1767d7a31c7f3583
Lars Schumann <LarsSchumann@mail.de>
2026-05-13 18:40:15 +0200

    docs: more elaborate examples

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,16 @@
 
 /// # Examples
 /// ```rust
-/// use auto_array::auto_array;
-///
-/// auto_array!(
-///     pub const A: [i32; _] = [1, 2, 3];
-///     static B: [u8; _] = [4, 4, 4, 4];
-/// );
-/// assert_eq!(A, [1, 2, 3]);
-/// assert_eq!(B, [4, 4, 4, 4]);
+/// auto_array! {
+///     // Additional attributes, docs, and visibility are supported.
+///     /// A const array.
+///     #[allow(unused)]
+///     const A: [u8; _] = [3, 3, 3];
+///     /// A static array with conditional compilation and pub(crate) visibility.
+///     pub(crate) static B: [u8; _] = [1, #[cfg(unix)] 2];
+/// }
+/// assert_eq!(A, [3, 3, 3]);
+/// assert_eq!(B, [1, #[cfg(unix)] 2]);
 /// ```
 #[macro_export]
 macro_rules! auto_array {
